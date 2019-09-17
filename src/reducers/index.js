9d92972c74cb0c50d8b77259c95408e6db7228f7
kumud12 <kumudbih12@gmail.com>
2019-09-17 17:05:36 +0530

    initial commit songs app

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const songReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Hello Buddy", duration: "5:15" },
+    { title: "No Smoking", duration: "3:05" },
+    { title: "No disturb", duration: "4:50" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songReducer,
+  selectedSong: selectedSongReducer
+});
